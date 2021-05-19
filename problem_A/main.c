3d4847b78c358e916a4bf5a62607f87e3c937069
root <karlbreit@gmail.com>
2021-05-19 13:35:35 +0000

    Done

--- problem_A/main.c
+++ problem_A/main.c
@@ -11,27 +11,36 @@ xTaskHandle printFinalCounterTaskHandle;
 
 SemaphoreHandle_t incTaskDone;
 SemaphoreHandle_t decTaskDone;
+SemaphoreHandle_t guardResource;
 
 volatile int counter = 0;
 
 void incCounterTask(void *pvParameters)
 {
+    xSemaphoreTake(guardResource, portMAX_DELAY);
+    printf("upcounter \n");
     int i;
     for(i = 0; i < 1e7; i++)
     {
 	counter = counter + 1;
     }
+    printf("upcounter %d \n",counter);
+     xSemaphoreGive(guardResource);
     xSemaphoreGive(incTaskDone); 
     vTaskDelete(incTaskHandle);
 }
 
 void decCounterTask(void *pvParameters)
 {
+    xSemaphoreTake(guardResource, portMAX_DELAY);
+    printf("downcounter \n");
     int i;
     for(i = 0; i < 1e7; i++ )
     {
 	counter = counter - 1;
     }
+    printf("downcounter %d \n",counter);
+     xSemaphoreGive(guardResource);
     xSemaphoreGive(decTaskDone); 
     vTaskDelete(decTaskHandle);
 }
@@ -48,6 +57,7 @@ int main(int argc, char **argv)
 {
     incTaskDone = xSemaphoreCreateBinary();
     decTaskDone = xSemaphoreCreateBinary();
+    guardResource = xSemaphoreCreateMutex();
     xTaskCreate(incCounterTask, "Increase Counter Task", configMINIMAL_STACK_SIZE, NULL, 1, &incTaskHandle);
     xTaskCreate(decCounterTask, "Decrease Counter Task", configMINIMAL_STACK_SIZE, NULL, 1, &decTaskHandle);
     xTaskCreate(printFinalCounterTask, "Print Final Counter Task", configMINIMAL_STACK_SIZE, NULL, 1, &printFinalCounterTaskHandle);
